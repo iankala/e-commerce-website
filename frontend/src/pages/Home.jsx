e3a75fc9b5439e771b436a2cbfd9a7cfbf12540a
iankala <iankala12814@gmail.com>
2026-02-24 08:03:27 +0300

    changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,2 +1,20 @@
-const Home = () => <div>Home Page</div>
+import React from 'react'
+import Hero from '../components/hero'
+import LatestCollection from '../components/latestCollection'
+import BestSeller from '../components/bestSeller'
+import OurPolicy from '../components/ourPolicy'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <Newsletter />
+    </div>
+  )
+}
+
 export default Home
